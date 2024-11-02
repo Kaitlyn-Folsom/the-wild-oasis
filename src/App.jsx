140e278529cbd741b69874f952bdf449a0c3a945
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2024-11-02 19:19:08 -0400

    setup page routes with browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import styled from 'styled-components';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
-import Heading from './ui/Heading';
-import Row from './ui/Row';
-import Button from './ui/Button';
-
-const StyledApp = styled.div`
-  padding: 20px;
-`;
+import Dashboard from './pages/Dashboard';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import Users from './pages/Users';
+import Settings from './pages/Settings';
+import Account from './pages/Account';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type='horizontal'>
-            <Heading as='h1'>Hello world!</Heading>
-            <div>
-              <Heading as='h2'>Check in and out</Heading>
-              <Button>Check in</Button>
-              <Button variation='secondary' size='medium'>
-                Check out
-              </Button>
-            </div>
-          </Row>
-          <Row>
-            <Heading as='h3'>Form</Heading>
-          </Row>
-        </Row>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to='dashboard' />} />
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='bookings' element={<Bookings />} />
+          <Route path='cabins' element={<Cabins />} />
+          <Route path='users' element={<Users />} />
+          <Route path='settings' element={<Settings />} />
+          <Route path='account' element={<Account />} />
+          <Route path='login' element={<Login />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
