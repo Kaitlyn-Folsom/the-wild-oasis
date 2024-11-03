cb74160e03317e311b8a4cdd3211223931e452e9
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2024-11-03 15:26:09 -0500

    functionality to create a new cabin

---
+++ src/ui/Input.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  font-size: 1.4rem;
+  padding: 0.8rem 1.2rem;
+  border: 1px solid var(--color-grey-300);
+  border-radius: 5px;
+  background-color: var(--color-grey-0);
+  box-shadow: var(--shadow-sm);
+  width: 100%;
+`;
+
+export default Input;
