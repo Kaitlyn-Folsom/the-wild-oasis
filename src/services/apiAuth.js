bb3c2df0fc0b23502c3b455ad4e14ad6d60503a5
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2024-11-20 20:07:04 -0500

    completed update user functionality

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from './supabase';
+import supabase, { supabaseUrl } from './supabase';
 
 export async function signup({ email, password, full_name, avatar }) {
   let { data, error } = await supabase.auth.signUp({
@@ -6,8 +6,8 @@ export async function signup({ email, password, full_name, avatar }) {
     password,
     options: {
       data: {
-        full_name,
-        avatar,
+        full_name: full_name,
+        avatar: avatar,
       },
     },
   });
@@ -34,8 +34,6 @@ export async function getCurrentUser() {
 
   const { data, error } = await supabase.auth.getUser();
 
-  console.log(data);
-
   if (error) throw new Error(error.message);
 
   return data?.user;
@@ -45,4 +43,33 @@ export async function logout() {
   const { error } = await supabase.auth.signOut();
 
   if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser({ password, full_name, avatar }) {
+  let updateData;
+
+  if (password) updateData = { password };
+  if (full_name) updateData = { data: { full_name: full_name } };
+
+  const { data, error } = await supabase.auth.updateUser(updateData);
+
+  if (error) throw new Error(error.message);
+
+  if (!avatar) return data;
+
+  const fileName = `avatar=${data.user.id}-${Math.random()}`;
+
+  const { error: storageError } = await supabase.storage.from('avatars').upload(fileName, avatar);
+
+  if (storageError) throw new Error(storageError.message);
+
+  const { data: updatedUser, error: errorUser } = await supabase.auth.updateUser({
+    data: {
+      avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+    },
+  });
+
+  if (errorUser) throw new Error(errorUser.message);
+
+  return updatedUser;
 }
