28069c66615b89e382f928523aeb3e2ff5d2d68f
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2024-11-22 20:54:52 -0500

    show empty component when no booking detail page found

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -10,6 +10,7 @@ import ButtonText from '../../ui/ButtonText';
 import Spinner from '../../ui/Spinner';
 import Modal from '../../ui/Modal';
 import ConfirmDelete from '../../ui/ConfirmDelete';
+import Empty from '../../ui/Empty';
 
 import { useMoveBack } from '../../hooks/useMoveBack';
 import { useBooking } from './useBooking';
@@ -31,6 +32,7 @@ function BookingDetail() {
   const navigate = useNavigate();
 
   if (isLoading) return <Spinner />;
+  if (!booking) return <Empty resource='booking' />;
 
   const { id: bookingId, status } = booking;
 
